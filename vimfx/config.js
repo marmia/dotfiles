69f8840abf0bcd28fb4ee996095d07a40102fe3b
marmia <marmia312@gmail.com>
2017-02-21 16:28:09 +0900

    1. 新規ファイルの追加 (vimfx/)

---
+++ vimfx/config.js
@@ -0,0 +1,11 @@
+
+vimfx.addKeyOverrides(
+  [ location => location.hostname === 'www.inoreader.com',
+    ['a', 'r', '.', '/', 'd', 'u', '<a-p>', '<a-c>', '<a-i>',
+     '1', '2', '3', '4', '=', '+', '-', '0', '<s-1>', '<s-2>', '<s-3>',
+     '?', '<escape>', 'j', 'k', 'n', 'p', '<up>', '<down>', '<left>', '<right>',
+     'o', 'v', 'b', 'w', 'm', '<s-a>', '<s-m>', '<c-m>', 'f', 's',
+     'e', 't', 'q', '<s-j>', '<s-k>', '<s-n>', '<s-p>', '<s-o>', '<s-x>', '<s-d>'
+    ]
+  ]
+)
